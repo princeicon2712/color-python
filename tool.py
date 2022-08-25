3f059e5748dd13776c86af18088eff7bbd99d4e0
Prince <91708065+princeicon2712@users.noreply.github.com>
2022-08-24 22:50:53 -0700

    pssword and name

---
+++ tool.py
@@ -0,0 +1,43 @@
+red="\033[0;31m"          # Red
+color_off="\033[0m"       # Text Reset
+green="\033[0;32m"        # Green
+print(green)
+print("""
+.______       _______        __       ___      __    __   __      
+|   _  \     |   ____|      |  |     /   \    |  |  |  | |  |     
+|  |_)  |    |  |__         |  |    /  ^  \   |  |  |  | |  |     
+|      /     |   __|  .--.  |  |   /  /_\  \  |  |  |  | |  |     
+|  |\  \----.|  |____ |  `--'  |  /  _____  \ |  `--'  | |  `----.
+| _| `._____||_______| \______/  /__/     \__\ \______/  |_______|
+""")
+print('''
+\033[1;92m i am hacker Rejaul karim prince 2000\n
+ROOT Phone pkg:\npkg install -y root-repo\n
+pkg install -y git tsu python wpa-supplicant pixiewps iw\n
+computer Linux pkg:\nsudo apt install -y python3 wpasupplicant iw wget\n
+One line Command:\nsudo python rejaul2712/rejaul.py -i wlan0 -K\n
+For Help Type:\nsudo python rejaul.py -i wlan0 -K\033[0m
+''')
+red="\033[0;31m"          # Red
+color_off="\033[0m"       # Text Reset
+green="\033[0;32m"        # Green
+print(green)
+import sys
+print("Welcom To SYLHET")
+
+user=("10203040")
+passw=("10203040")
+
+us=str(input("Input your usarname:- "))
+pas=str(input("input your password:-"))
+
+if us==user and pas==passw:
+	pass
+	print("100%")
+else:
+	print("Rong")
+	sys.exit()
+	
+print("")
+print("welcom to wifi hack")
+
